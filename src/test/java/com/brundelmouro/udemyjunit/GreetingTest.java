b88d9dec23c1376e90e03eea4a6a101a6da405aa
Bruna Delmouro <brunadelmouro@gmail.com>
2022-06-02 14:43:13 -0300

    JUnit 5 Test Instance Lifecycle Demo

--- src/test/java/com/brundelmouro/udemyjunit/GreetingTest.java
+++ src/test/java/com/brundelmouro/udemyjunit/GreetingTest.java
@@ -1,13 +1,17 @@
 package com.brundelmouro.udemyjunit;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GreetingTest {
 
     private Greeting greeting;
+
+    @BeforeAll
+    public static void beforeClass(){
+        System.out.println("Before - I am only called once!!");
+    }
 
     @BeforeEach
     void setUp() {
@@ -24,5 +28,20 @@ class GreetingTest {
     @Test
     void testHelloWorld() {
         System.out.println(greeting.helloWorld("Bruna"));
+    }
+
+    @Test
+    void testHelloWorld2() {
+        System.out.println(greeting.helloWorld("Alex"));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("In After Each...");
+    }
+
+    @AfterAll
+    public static void afterClass(){
+        System.out.println("After - I am only called once!!");
     }
 }
