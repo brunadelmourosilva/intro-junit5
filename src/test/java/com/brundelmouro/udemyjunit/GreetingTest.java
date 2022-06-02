1ac5e515f18f240fc99c226915cd540c2692f801
Bruna Delmouro <brunadelmouro@gmail.com>
2022-06-02 14:35:35 -0300

    Initializing Properties with JUnit 5 @BeforeEach

--- src/test/java/com/brundelmouro/udemyjunit/GreetingTest.java
+++ src/test/java/com/brundelmouro/udemyjunit/GreetingTest.java
@@ -1,22 +1,28 @@
 package com.brundelmouro.udemyjunit;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GreetingTest {
 
+    private Greeting greeting;
+
+    @BeforeEach
+    void setUp() {
+        System.out.println("in Before Each....");
+
+        greeting = new Greeting();
+    }
+
     @Test
     void helloWorld() {
-        Greeting greeting = new Greeting();
-
         System.out.println(greeting.helloWorld());
     }
 
     @Test
     void testHelloWorld() {
-        Greeting greeting = new Greeting();
-
         System.out.println(greeting.helloWorld("Bruna"));
     }
 }
