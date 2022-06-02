594e40daccf10dd624bbb2bc0cd0a81324837813
Bruna Delmouro <brunadelmouro@gmail.com>
2022-06-02 14:10:29 -0300

    Inital commit

---
+++ src/main/java/com/brundelmouro/udemyjunit/UdemyjunitApplication.java
@@ -0,0 +1,13 @@
+package com.brundelmouro.udemyjunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyjunitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyjunitApplication.class, args);
+	}
+
+}
